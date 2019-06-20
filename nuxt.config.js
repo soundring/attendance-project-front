74327ca6a8fcebe994108be0a7df6f7b6921d098
soundring <soundringing@yahoo.co.jp>
2019-06-20 12:53:09 +0900

    時刻コンポーネントの作成

--- nuxt.config.js
+++ nuxt.config.js
@@ -34,6 +34,7 @@ module.exports = {
         })
       }
     }
-  }
+  },
+  plugins: ['@/plugins/dayjs']
 }
 
--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,13 @@
 <template>
-  
+  <displayTime />
 </template>
 
 <script>
+import displayTime from '~/components/Display_time.vue'
 export default {
-  
+  components: {
+    displayTime,
+  }
 }
 </script>
 
