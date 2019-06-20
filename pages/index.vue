3c515a8e0bcb9bfb7f2360f9aefe698bb5eccd03
soundring <soundringing@yahoo.co.jp>
2019-06-20 13:54:03 +0900

    出勤、退勤ボタンの追加、時刻コンポーネントのリネーム

--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,23 @@
 <template>
+<div class="contents">
   <displayTime />
+  <attendanceButton />
+</div>
 </template>
 
 <script>
-import displayTime from '~/components/Display_time.vue'
+import displayTime from '~/components/DisplayTime.vue'
+import attendanceButton from '~/components/AttendanceButton'
 export default {
   components: {
     displayTime,
+    attendanceButton,
   }
 }
 </script>
 
 <style>
-
+.contents {
+  display: flex;
+}
 </style>
